de7197fba855c0bd9643c772f97b9cdb7b7896a2
Christopher Ducey <chrisducey01@me.com>
2019-09-16 20:11:50 -0700

    Added js object for Aeris weather api for ajax calls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,32 @@
 const algoliaApiKey = "420478f8416cbf67fc5dc4b1617e298a";
 const algoliaAppId  = "pl4NIPBVHT19";
 
+const weatherAccessId = "IhoPBape6zamvrXhAop7j";
+const weatherSecretKey = "XfKCLeB7QSZnVmsSSrlqL5abBWmH1kLv4GiHMpWB"
+const weatherApiKey = "1918920b03msha6fce5bb1344417p16cfcbjsnd91e277e0013";
+const weatherApiBaseUrl = "https://api.aerisapi.com/observations/";
+
+const aerisWeather = {
+    apiKey: "XfKCLeB7QSZnVmsSSrlqL5abBWmH1kLv4GiHMpWB",
+    accessId: "IhoPBape6zamvrXhAop7j",
+    currentWeatherBaseUrl: "https://api.aerisapi.com/observations/",
+    getCurrentWeather(location,callback){
+        let queryUrl = `${this.currentWeatherBaseUrl}${location}?&format=json&filter=allstations&limit=1&client_id=${this.accessId}&client_secret=${this.apiKey}`; 
+        console.log(queryUrl);
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response){
+          callback(response);
+        });
+    }
+};
+
 let latLng;  //latitude and longitude in an object returned from the suggestion from Algolia places
+
+function logOutToConsole(obj){
+    console.log(obj);
+}
 
 $(document).ready(function(){
 
@@ -16,4 +41,5 @@ $(document).ready(function(){
       //When user selects a suggested address, save off the latitude and longitude
       placesAutocomplete.on('change', e => {latLng = e.suggestion.latlng;});
 
+      aerisWeather.getCurrentWeather("33.4486,-112.077",logOutToConsole);  //testing with lat/long for Phoenix
 });
