a5da3207d10ae5ad054615ba6c1e96f5ee6c0414
Jesus Esquer <jme2791@gmail.com>
2019-09-17 20:13:41 -0700

    erased comments for star ratings

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,4 @@
-// $("#starOne").on ("click", function(){
-//     $("#starOne").html("&#9733;");
-// });
-// $("#starTwo").on ("click", function(){
-//     $("#starTwo").html("&#9733;&#9733;");
-// });
-// $("#starThree").on ("click", function(){
-//     $("#starThree").html("&#9733;&#9733;&#9733;");
-// });
-// $("#starFour").on ("click", function(){
-//     $("#starFour").html("&#9733;&#9733;&#9733;&#9733;");
-// });
-//$("#starFive").on("click", function () {
- //   $("#starFive").html("&#9733;&#9733;&#9733;&#9733;&#9733;");
-//});
-// Add active class to the current button (highlight it)
-// var header = $("#ratingContainer");
-// var btns = $(".star");
-// for (var i = 0; i < btns.length; i++) {
-//     btns[i].addEventListener("click", function () {
-//         var current = document.getElementsByClassName("active");
-//         current[0].className = current[0].className.replace(" active", "");
-//         this.className += " active";
-//     });
-// }
+
 const toogleActive = function(button){
     var state = button.attr("data-state");
     if(state == "inactive"){
